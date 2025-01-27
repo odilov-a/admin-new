b88b5f4b515ef193d09327cc79bbbbb3a0ac3435
Akbar Odilov <akbarjonodilov01@gmail.com>
2025-01-27 12:37:30 +0500

    add CoutStudents component to display student count and rename Subject to Problem

--- src/pages/chart/index.tsx
+++ src/pages/chart/index.tsx
@@ -1,10 +1,12 @@
 import { Row, Col } from "antd";
 import LanguageDistributionChart from "./languageDistributionChart";
+import CoutStudents from "./coutStudents";
 
 const Dashboard = () => {
   return (
     <Row gutter={16}>
       <Col span={12}>
+        <CoutStudents />
         <LanguageDistributionChart />
       </Col>
     </Row>
